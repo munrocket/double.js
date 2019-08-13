80ca2772f4f3eca9c6b1f3f45579aaeccf0f0fe8
munrocket <munrocket@protonmail.com>
2019-08-13 14:44:03 +0400

    simplify rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,16 @@
 import babel from 'rollup-plugin-babel';
 
 export default [
-	{
-		input: 'src/double.js',
-		output: { file: "dist/double.cjs.js", format: 'cjs' }
-	},
-	{
-		input: 'src/double.js',
-		output: { file: "dist/double.iife.js", name: 'Double', format: 'iife' }
-	},
-	{
-		input: 'src/double.js',
-		output: { file: "dist/double.iife.es5.js", name: 'Double', format: 'iife' },
-		plugins: [babel({exclude: 'node_modules/**'})]
-	}
+  {
+    input: 'src/double.js',
+    output: [
+      { file: "dist/double.cjs.js", format: 'cjs' },
+      { file: "dist/double.iife.js", name: 'Double', format: 'iife' }
+    ]
+  },
+  {
+    input: 'src/double.js',
+    output: { file: "dist/double.iife.es5.js", name: 'Double', format: 'iife' },
+    plugins: [babel({exclude: 'node_modules/**'})]
+  }
 ];
